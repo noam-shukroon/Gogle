76e3ee34b6ec4258426c9a435e21c469a594c777
noam-shukroon <77461520+noam-shukroon@users.noreply.github.com>
2026-02-19 04:03:18 +0200

    IM HIGHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- gogle/src/App.tsx
+++ gogle/src/App.tsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import styled from 'styled-components'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function gogle() {
 
+  const Superdiv = styled.div`
+    display: flex;
+    height: 100vh;
+    width: 100vw;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+  `
+  const Topbar = styled.div`
+      display: flex;
+      height: 13vh;
+      width: 100%;
+      top:0;
+      z-index: 1000;
+      background-color: #1a73e8;
+
+      justify-content: flex-end;
+      align-items: center;
+      
+      gap: 1rem;
+      `
+
+  const TopBarLink = styled.h1`
+    color: #ffffff;
+    font-size: 0.8rem;
+    text-align: center;
+
+    &:hover {
+    text-decoration: underline;
+    cursor: pointer;
+    }
+  `
+    
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Superdiv> 
+    <Topbar>  
+      <TopBarLink>Gmail</TopBarLink>
+      <TopBarLink>Images</TopBarLink>
+    </Topbar>
+    </Superdiv>
   )
 }
 
-export default App
+export default gogle
